62b5a9c63d5cd06cce1031f17edd2e69276f34b7
maker-dasu <winter7574@gmail.com>
2022-06-20 12:00:00 +0900

    [baekjoon] 18108번

---
+++ src/boj/CalcYear.java
@@ -0,0 +1,14 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CalcYear {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+
+        System.out.println(A - 543);
+    }
+}
