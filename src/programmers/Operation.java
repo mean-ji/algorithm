7cb39e64fa048104372603a87bf4a62208cc51bd
mean-ji <winter7574@gmail.com>
2022-08-18 12:00:00 +0900

    두 수의 차

--- src/programmers/Operation.java
+++ src/programmers/Operation.java
@@ -8,4 +8,13 @@ public class Operation {
     public int plus(int num1, int num2) {
         return num1 + num2;
     }
+
+    public int minus(int num1, int num2) {
+        int answer = 0;
+        boolean val = (-50000 <= num1 && 50000 >= num1) && (-50000 <= num2 && 50000 >= num2);
+        if (val) {
+            return num1 - num2;
+        }
+        return answer;
+    }
 }
