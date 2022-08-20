0e6578ee0606e980f9ad0f78f9f9f34309a4df04
mean-ji <winter7574@gmail.com>
2022-08-20 12:00:00 +0900

    문제 풀이 방법 추가 정리

--- src/programmers/NotFinishAthletes.java
+++ src/programmers/NotFinishAthletes.java
@@ -3,6 +3,9 @@ import java.util.Arrays;
 
 public class NotFinishAthletes {
     public String solution(String[] participant, String[] completion) {
+        /**
+         * 방법1. loop로 간단하게 구현
+         */
         String answer = "";
         // 1. 두 배열 모두 알파벳 순으로 정렬
         Arrays.sort(participant);
@@ -19,5 +22,27 @@ public class NotFinishAthletes {
         }
 
         return answer.equals("") ? participant[participant.length - 1] : answer;
+
+        /**
+         * 방법2. HashMap 사용
+         */
+        /**
+         * String answer = "";
+         * HashMap<String, Integer> map = new HashMap<>();
+         * for (String player : participant)
+         *     map.put(player, map.getOrDefault(ployer, 0) + 1);
+         * for (String player : completion)
+         *     map.put(player, map.get(ployer) - 1);
+         * Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
+         *
+         * while(iter.hashNext()) {
+         *     Map.Entry<String, Integer> entry = iter.next();
+         *     if (entry.getValue() != 0) {
+         *         answer = entry.getKey();
+         *         break;
+         *     }
+         * }
+         * return answer;
+         */
     }
 }
