0dcc0faa0de1bc3689442f97f9c55fe7d1489829
mean-ji <winter7574@gmail.com>
2022-08-31 23:00:00 +0900

    [boj] 11021번

---
+++ src/boj/APlusBMinus7.java
@@ -0,0 +1,24 @@
+package boj;
+
+import java.io.*;
+
+public class APlusBMinus7 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int caseT = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= caseT; i++) {
+            String line = br.readLine();
+            int left = Integer.parseInt(line.split(" ")[0]);
+            int right = Integer.parseInt(line.split(" ")[1]);;
+            int total = left + right;
+
+            bw.write("Case #" + i + ": " + total);
+            bw.newLine();
+        }
+
+        br.close();
+        bw.close();
+    }
+}
