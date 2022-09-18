c2f70fed8674738451f36793a446ef2fb8a3a3c7
mean-ji <winter7574@gmail.com>
2022-09-18 13:00:00 +0900

    [boj] 10718번

---
+++ src/boj/WeLoveKriii.java
@@ -0,0 +1,12 @@
+package boj;
+
+/**
+ * 10718번
+ */
+public class WeLoveKriii {
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            System.out.println("강한친구 대한육군");
+        }
+    }
+}
