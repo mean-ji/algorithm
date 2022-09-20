1c05fd34625816d9c7533dc2d77a4c4ff8d86fcd
mean-ji <winter7574@gmail.com>
2022-09-20 13:00:00 +0900

    [boj] 1001번

---
+++ src/boj/AMinusB.java
@@ -0,0 +1,21 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 1001번
+ */
+public class AMinusB {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A - B);
+    }
+}
