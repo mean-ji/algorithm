76b5c1c6b55cfb6504dfc6bb2360085366dddab1
mean-ji <winter7574@gmail.com>
2022-09-24 13:00:00 +0900

    [boj] 10998번

---
+++ src/boj/AMultB.java
@@ -0,0 +1,21 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 10998번
+ */
+public class AMultB {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A * B);
+    }
+}
