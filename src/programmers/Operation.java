c1286ee223157d23ae56ae9a270157bf6fcdabec
mean-ji <winter7574@gmail.com>
2022-12-24 12:00:00 +0900

    두 수의 나눗셈

--- src/programmers/Operation.java
+++ src/programmers/Operation.java
@@ -12,9 +12,11 @@ public class Operation {
     public int minus(int num1, int num2) {
         int answer = 0;
         boolean val = (-50000 <= num1 && 50000 >= num1) && (-50000 <= num2 && 50000 >= num2);
+
         if (val) {
             return num1 - num2;
         }
+
         return answer;
     }
 
@@ -24,6 +26,21 @@ public class Operation {
 
         if (val) {
             return num1 / num2;
+        }
+
+        return answer;
+    }
+
+    public int divide2(int num1, int num2) {
+        int answer = 0;
+        boolean val = (0 < num1 && 100 >= num1) && (0 < num2 && 100 >= num2);
+
+        if (val) {
+            double num1Double = num1;
+            double num2Double = num2;
+            double result = (num1Double / num2Double) * 1000;
+
+            answer = (int) Math.floor(result);
         }
 
         return answer;
