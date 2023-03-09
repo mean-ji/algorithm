cd98b9c107105dcb3a3a446bb0f3ede063ae6d32
mean-ji <winter7574@gmail.com>
2023-03-09 12:00:00 +0900

    Concatenation

--- src/leetcode/Concatenation.java
+++ src/leetcode/Concatenation.java
@@ -1,2 +1,22 @@
-package leetcode;public class Concatenation {
+package leetcode;
+
+import java.util.Arrays;
+
+public class Concatenation {
+    public static void main(String[] args) {
+        Concatenation c = new Concatenation();
+        int[] nums = {1,2,1};
+        System.out.println(Arrays.toString(c.getConcatenation(nums)));
+    }
+
+    public int[] getConcatenation(int[] nums) {
+        int[] result = new int[nums.length * 2];
+
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = nums[i];
+            result[i + nums.length] = nums[i];
+        }
+
+        return result;
+    }
 }
