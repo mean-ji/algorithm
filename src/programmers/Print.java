0d8d1edb710f8b1d27e2ed3d2f0e616875edfb5a
mean-ji <winter7574@gmail.com>
2023-04-11 12:00:00 +0900

    print

---
+++ src/programmers/Print.java
@@ -0,0 +1,10 @@
+package programmers;
+
+import java.util.Scanner;
+public class Print {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
