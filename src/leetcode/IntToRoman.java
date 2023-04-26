803b4107b9b5d33927c2e99fc7f9d830824aa4bc
mean-ji <winter7574@gmail.com>
2023-04-26 12:00:00 +0900

    classfile

---
+++ src/leetcode/IntToRoman.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class IntToRoman {
+    public static void main(String[] args) {
+        IntToRoman i = new IntToRoman();
+        System.out.println(i.intToRoman(58));
+    }
+
+    public String intToRoman(int num) {
+    }
+}
