185722d4e36f1f2bbe791b9837e8e2fad0f04074
mean-ji <winter7574@gmail.com>
2023-05-12 12:00:00 +0900

    subs

--- src/programmers/Print.java
+++ src/programmers/Print.java
@@ -21,4 +21,13 @@ public class Print {
     public int add(int num1, int num2) {
         return num1 + num2;
     }
+
+    public int subs(int num1, int num2) {
+        int answer = 0;
+        boolean val = (-50000 <= num1 && 50000 >= num1) && (-50000 <= num2 && 50000 >= num2);
+        if (val) {
+            return num1 - num2;
+        }
+        return answer;
+    }
 }
