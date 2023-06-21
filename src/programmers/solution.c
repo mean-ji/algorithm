8013f4e33878aa622d489fe14ce2075d2e9536b2
mean-ji <winter7574@gmail.com>
2023-06-21 12:00:00 +0900

    print 약수의 합 use c++

--- src/programmers/solution.c
+++ src/programmers/solution.c
@@ -73,6 +73,20 @@ char* subak(int n) {
     free(answer);
 }
 
+int solution(int n) {
+    int c[3000] = {0,};
+
+    for(int i = n; i > 0; i --){
+        if(n%i == 0)
+            c[i] = i;
+    }
+    int answer = 0;
+    for(int i = 0; i < 3000 ; i++){
+        answer = answer + c[i];
+    }
+    return answer;
+}
+
 int main(){
     int a , b;
     printf("2016년 a월 b일을 구해봅시다!!");
@@ -89,4 +103,8 @@ int main(){
     scanf("%d", &n);
     solution(n);
 
+    int n;
+    printf("약수의 합을 구합시당다라당당");
+    scanf("%d", &n);
+    solution(n);
 }
