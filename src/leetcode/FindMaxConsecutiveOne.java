13b98c985151e5951ab11751e810d5c469204969
mean-ji <winter7574@gmail.com>
2023-07-18 12:00:00 +0900

    findMaxConsecutiveOnes

---
+++ src/leetcode/FindMaxConsecutiveOne.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class FindMaxConsecutiveOne {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int curSum = 0;
+        int result = 0;
+
+        for (int i : nums) {
+            if (i == 0) {
+                result = Math.max(curSum, result);
+                curSum = 0;
+            } else {
+                curSum += i;
+            }
+        }
+
+        return Math.max(curSum, result);
+    }
+}
