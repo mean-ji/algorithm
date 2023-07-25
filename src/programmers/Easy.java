847b159074af9e5f147d8cd77576a884f1d50997
mean-ji <winter7574@gmail.com>
2023-07-25 12:00:00 +0900

    문자열 섞기

--- src/programmers/Easy.java
+++ src/programmers/Easy.java
@@ -129,4 +129,12 @@ public class Easy {
     public void printSpecialString() {
         System.out.println("!@#$%^&*(\\\'\"<>?:;");
     }
+
+    public String mix(String str1, String str2) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < str1.length(); i++) {
+            answer.append(str1.charAt(i)).append(str2.charAt(i));
+        }
+        return answer.toString();
+    }
 }
