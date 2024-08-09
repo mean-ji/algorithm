c09ca70a615ce8a9d2acc0f4dbc1b8016dfa0d55
mean-ji <winter7574@gmail.com>
2024-08-09 12:00:00 +0900

    송아지 찾기1 강의

--- src/inflearn_study/FindTheCalf.java
+++ src/inflearn_study/FindTheCalf.java
@@ -5,35 +5,74 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class FindTheCalf {
-    public int[] dist = {5, -1, 1};
-    public int bfs(int start, int end, int[] arr) {
-        int answer = 0;
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
+    // me
+//    public int[] dist = {5, -1, 1};
+//    public int bfs(int start, int end, int[] arr) {
+//        int answer = 0;
+//        Queue<Integer> q = new LinkedList<>();
+//        q.offer(start);
+//
+//        while(!q.isEmpty()) {
+//            int index = q.poll();
+//            answer++;
+//            if (end < index) {
+//                q.offer(index + dist[1]);
+//            }
+//            for (int i = 0; i < dist.length; i++) {
+//                int j = dist[i];
+//                if (index + j < end) {
+//                    q.offer(index + j);
+//                    break;
+//                } else if (index + j == end) {
+//                    q.poll();
+//                    break;
+//                } else {
+//                    if (end - index < 3 && end - index > 0) {
+//                        i++;
+//                    }
+//                }
+//            }
+//        }
+//
+//        return answer;
+//    }
+//    public static void main(String[] args) {
+//        FindTheCalf T = new FindTheCalf();
+//        Scanner kb = new Scanner(System.in);
+//        int S = kb.nextInt();
+//        int E = kb.nextInt();
+//        int[] arr = new int[E + 1];
+//        System.out.println(T.bfs(S, E, arr));
+//    }
 
-        while(!q.isEmpty()) {
-            int index = q.poll();
-            answer++;
-            if (end < index) {
-                q.offer(index + dist[1]);
-            }
-            for (int i = 0; i < dist.length; i++) {
-                int j = dist[i];
-                if (index + j < end) {
-                    q.offer(index + j);
-                    break;
-                } else if (index + j == end) {
-                    q.poll();
-                    break;
-                } else {
-                    if (end - index < 3 && end - index > 0) {
-                        i++;
+    // lecture
+    int answer = 0;
+    int[] dis = {1, -1, 5};
+    int[] ch;
+    Queue<Integer> q = new LinkedList<>();
+
+    public int bfs(int s, int e) {
+        ch = new int[10001];
+        ch[s] = 1;
+        q.offer(s);
+        int L = 0;
+        while (!q.isEmpty()) {
+            int len = q.size();
+            for (int i = 0; i < len; i++) {
+                int x = q.poll();
+//                if (x == e) return L;
+                for (int j = 0; j < 3; j++) {
+                    int nx = x + dis[j];
+                    if (nx == e) return L + 1;
+                    if (nx >= 1 && nx <= 10000 && ch[nx] == 0) {
+                        ch[nx] = 1;
+                        q.offer(nx);
                     }
                 }
             }
+            L++;
         }
-
-        return answer;
+        return L;
     }
     public static void main(String[] args) {
         FindTheCalf T = new FindTheCalf();
@@ -41,6 +80,6 @@ public class FindTheCalf {
         int S = kb.nextInt();
         int E = kb.nextInt();
         int[] arr = new int[E + 1];
-        System.out.println(T.bfs(S, E, arr));
+        System.out.println(T.bfs(S, E));
     }
 }
