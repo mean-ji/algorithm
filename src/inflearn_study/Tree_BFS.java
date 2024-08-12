b586ebd0337b1daa0b0b76a9368e728ee73f1dec
mean-ji <winter7574@gmail.com>
2024-08-12 12:00:00 +0900

    말단 노드까지 가장 짧은 경로(BFS)

---
+++ src/inflearn_study/Tree_BFS.java
@@ -0,0 +1,49 @@
+package inflearn_study;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node4 {
+    int data;
+    Node4 lt, rt;
+    public Node4(int val) {
+        data = val;
+        lt = rt = null;
+    }
+}
+
+// me
+public class Tree_BFS {
+    Node4 root;
+    public int bfs(Node4 root) {
+        Queue<Node4> q = new LinkedList<>();
+        q.offer(root);
+        int L = 0;
+        int cnt = 0;
+        while(!q.isEmpty()) {
+            Node4 tmp = q.poll();
+            cnt ++;
+            if (cnt > Math.pow(2, L)) {
+                cnt = 0;
+                L++;
+            }
+            if (tmp.lt != null && tmp.rt != null) {
+                q.offer(tmp.lt);
+                q.offer(tmp.rt);
+            } else {
+                return L;
+            }
+        }
+        return L;
+    }
+
+    public static void main(String[] args) {
+        Tree_BFS T = new Tree_BFS();
+        T.root = new Node4(1);
+        T.root.lt = new Node4(2);
+        T.root.rt = new Node4(3);
+        T.root.lt.lt = new Node4(4);
+        T.root.lt.rt = new Node4(5);
+        System.out.println(T.bfs(T.root));
+    }
+}
